610db7f8a9b267e89cf626e2367df409b8defe52
Hoang-Anh DANG <32315060+hoanganhdang@users.noreply.github.com>
2018-01-17 23:48:57 +0700

    Add files via upload

---
+++ BH1750FVI_I2Clib.cpp
@@ -0,0 +1,75 @@
+#include "BH1750FVI_I2Clib.h"
+#include "Arduino.h"
+
+BH1750FVI::BH1750FVI(){  
+  
+ }
+  
+  void BH1750FVI::begin(void){
+    I2c.begin();
+	I2c.timeOut(3000);
+    I2CWriteTo(Power_On ); //Turn it On
+    pinMode(AddrPin,OUTPUT);
+    digitalWrite(AddrPin,HIGH);      
+  }
+  void BH1750FVI::Sleep(void){
+    I2CWriteTo(Power_Down ); //Turn it off , Reset operator won't work in this mode
+  }
+  void BH1750FVI::Reset(void){
+    I2CWriteTo(Power_On ); //Turn it on again
+    I2CWriteTo(reset ); //Reset  
+  }
+  void BH1750FVI::SetAddress(uint8_t add){
+    switch (add){
+      case Device_Address_L:
+      address_value=Device_Address_L;
+      digitalWrite(AddrPin,LOW);
+      state=false;
+      break;
+      case Device_Address_H:
+      address_value=Device_Address_H;
+      digitalWrite(AddrPin,HIGH);
+      state=true;
+      break;
+    }
+    
+  }
+  void BH1750FVI::SetMode(uint8_t MODE){
+    switch(MODE){
+        case Continuous_H_resolution_Mode:
+        break;
+        case Continuous_H_resolution_Mode2:
+        break;
+        case Continuous_L_resolution_Mode:       
+        break;
+        case OneTime_H_resolution_Mode:
+        break;
+        case OneTime_H_resolution_Mode2:
+        break;
+        case OneTime_L_resolution_Mode:  
+        break;
+    }
+    delay(10);
+    I2CWriteTo(MODE);
+  }
+  
+  uint16_t BH1750FVI::GetLightIntensity(void){
+  uint16_t Intensity_value;
+  byte buf [2];
+  if (I2c.read (address_value, sizeof buf, buf) == 0)
+  {
+    Intensity_value = buf [0] << 8 | buf [1];
+  }
+  else
+  {
+	Intensity_value = 0;
+    Serial.println ("BH1750 not responding");
+  }
+  Intensity_value=Intensity_value/1.2;
+  return Intensity_value;
+    
+  }
+  
+  void BH1750FVI::I2CWriteTo(uint8_t DataToSend){
+    I2c.write(address_value, DataToSend);
+  }
